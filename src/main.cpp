51351a3a150becfbaad7f3227ea421e09cac7a59
Oscar Gledel <ogledel@gmail.com>
2025-07-02 23:31:36 +0200

    adding scene loading on command launch

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "Application.hpp"
 #include "SceneParser.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
 
     Application app;
-    app.run("resources/default_scene.json");
+    if(argc < 2) {
+        std::cout << "[WARNING] No scene file specified for this application, loading default scene" << std::endl;
+        app.run("resources/default_scene.json");
+    } else if (argc == 2) {
+        app.run(argv[1]);
+    } else {
+        std::cout << "[ERROR] Two many arguments provided, expected one : name of the scene file to load" << std::endl;
+    }
 
     return 0;
 }
