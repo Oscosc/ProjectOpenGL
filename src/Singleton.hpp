62869f4377368a83f9802985ec5755d637c1f3e2
Oscar GLEDEL <oscar.gledel@cnes.fr>
2025-07-03 17:01:07 +0200

    Documentation of: - SceneParser - ShaderManager - Singleton - Sphere

--- src/Singleton.hpp
+++ src/Singleton.hpp
@@ -1,18 +1,40 @@
 #pragma once
 
+/**
+ * @brief Generic template class defined to construct Singleton.
+ */
 template<typename T>
 class Singleton
 {
 public:
+    
+    /**
+     * @brief Return the instance of the singleton (create it if doesn't exist).
+     */
     static T& getInstance() {
         static T instance;
         return instance;
     }
 
+    /**
+     * @brief Removed copy constructor.
+     */
     Singleton(const Singleton&) = delete;
+
+    /**
+     * @brief Removed move constructor.
+     */
     Singleton& operator=(const Singleton&) = delete;
 
 protected:
+
+    /**
+     * @brief Constructor for inherited class. 
+     */
     Singleton() = default;
+
+    /**
+     * @brief Destructor for inherited class.
+     */
     ~Singleton() = default;
 };
--- src/Sphere.hpp
+++ src/Sphere.hpp
@@ -8,16 +8,35 @@
 #define DEFAULT_STACKS 64
 #define DEFAULT_SECTORS 64
 
+/**
+ * @brief Class used to define a Sphere object that can be rendered on the screen.
+ */
 class Sphere : public Object
 {
 public:
+
+    /**
+     * @brief Construct Vertices of a sphere and save it as an object
+     * 
+     * @param radius Radius of the sphere to create
+     * @param transform Sphere default transformation
+     * @param material Sphere default material
+     */
     Sphere(float radius,
         Transform transform = DEFAULT_OBJECT_TRANSFORM,
         Material material = DEFAULT_OBJECT_MATERIAL
     );
 
+    /**
+     * @brief Draw sphere in the scene (in consequent on the linked app window).
+     * 
+     * @param scene scene where the sphere is rendered
+     */
     void draw(Scene* scene) override;
 
+    /**
+     * @brief Gives the radius of the sphere.
+     */
     float getRadius() const;
 
 private:
